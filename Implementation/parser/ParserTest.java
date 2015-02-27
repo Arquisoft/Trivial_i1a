13f372f253e6569dacc4a04c964debe96062adce
Ainhoa Longo Pérez <uo230409@uniovi.es>
2015-02-27 18:59:49 +0100

    Parser test developed

---
+++ Implementation/parser/ParserTest.java
@@ -0,0 +1,34 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import quizStructure.Answer;
+
+public class ParserTest {
+
+	@Test
+	public void testParse() {
+		String file="Implementation/data/question.gift";
+		List<Answer> list = new ArrayList<Answer>();
+		
+		list.add(new Answer("Hideaki Anno",true));
+		list.add(new Answer("Toriyama",false));
+		
+		ParserGIFT parse = new ParserGIFT(file);
+		parse.parse();
+		assertEquals(parse.questions.size(), 1);
+		assertEquals(parse.questions.get(0).getTitle(), "Title");
+		assertEquals(parse.questions.get(0).getAnswers().get(0).getText(), list.get(0).getText());
+		assertEquals(parse.questions.get(0).getAnswers().get(1).getText(), list.get(1).getText());
+
+
+		assertEquals(parse.questions.get(0).getAnswers().get(0).isCorrectAnswer(), list.get(0).isCorrectAnswer());
+		assertEquals(parse.questions.get(0).getAnswers().get(1).isCorrectAnswer(), list.get(1).isCorrectAnswer());
+	}
+
+}
