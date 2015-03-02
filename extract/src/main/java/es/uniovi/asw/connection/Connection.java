1c7e6f80c364ba2855071af570b7d93ded54ef9e
Edgar Herrero <kaotixpro@gmail.com>
2015-03-02 23:40:58 +0100

    Warning solved in DB connection method.

--- extract/src/main/java/es/uniovi/asw/connection/Connection.java
+++ extract/src/main/java/es/uniovi/asw/connection/Connection.java
@@ -6,29 +6,28 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 
 public class Connection {
 
 	public static void connection() {
 		try {
-		Mongo conn = new Mongo();
-		
-		DB db = conn.getDB("Questions");
-		
-		DBCollection coll = db.getCollection("Questions");
-		
-		BasicDBObject query = new BasicDBObject();
-		query.put("id", 1001);
-		DBCursor cursor = coll.find(query);
-		while (cursor.hasNext()) {
-		System.out.println(cursor.next());
-		}
+
+			DB db = (new MongoClient("localhost", 27017)).getDB("Questions");
+
+			DBCollection coll = db.getCollection("Questions");
+
+			BasicDBObject query = new BasicDBObject();
+			query.put("id", 1001);
+			DBCursor cursor = coll.find(query);
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
 		} catch (UnknownHostException e) {
-		e.printStackTrace();
-		} catch (MongoException  e) {
-		e.printStackTrace();
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
 		}
-		}
+	}
 }
