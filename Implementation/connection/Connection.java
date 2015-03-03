3fa2586dd5c40145c3e8d9578f7c900c3ba864c5
Pradastur <pradastur@gmail.com>
2015-03-03 20:58:13 +0100

    Modification of the Connection class

--- Implementation/connection/Connection.java
+++ Implementation/connection/Connection.java
@@ -1,17 +1,19 @@
-package es.uniovi.asw.connection;
-
+package Connection;
 import java.net.UnknownHostException;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
+import com.mongodb.util.JSON;
 
 public class Connection {
 
-	public static void connection() {
+	public static void connection(String contentToTransform) {
 		try {
 		Mongo conn = new Mongo();
 		
@@ -20,8 +22,19 @@ public class Connection {
 		DBCollection coll = db.getCollection("Questions");
 		
 		BasicDBObject query = new BasicDBObject();
-		query.put("id", 1001);
+		
+		query.put("question", 1);
+		
+		coll.createIndex(query, "question", true);
+		
 		DBCursor cursor = coll.find(query);
+		
+		BasicDBList list= (BasicDBList) JSON.parse(contentToTransform);
+		
+		for(Object obj:list){
+			coll.insert((DBObject)obj);
+		}
+		
 		while (cursor.hasNext()) {
 		System.out.println(cursor.next());
 		}
