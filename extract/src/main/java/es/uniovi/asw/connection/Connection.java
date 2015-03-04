63d0a183d3088f3a4242b3caeb2efe2f12c8989b
Pradastur <pradastur@gmail.com>
2015-03-04 22:56:34 +0100

    Added the new Connection class

--- extract/src/main/java/es/uniovi/asw/connection/Connection.java
+++ extract/src/main/java/es/uniovi/asw/connection/Connection.java
@@ -2,8 +2,6 @@ package es.uniovi.asw.connection;
 
 import java.net.UnknownHostException;
 
-import org.mongojack.JacksonDBCollection;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -21,22 +19,6 @@ public class Connection {
 			DB db = (new MongoClient("localhost", 27017)).getDB("Questions");
 
 			DBCollection coll = db.getCollection("Questions");
-			
-			// Código de ejemplo de MongoJack, que es lo que se usa
-			// para convertir POJO (Plain Old Java Object) a JSON
-			// y almacenarlo en la base de datos.
-			
-			// JacksonDBCollection<Question, String> collection = JacksonDBCollection
-			//		.wrap(coll, Question.class, String.class);
-			// MyObject myObject = ...
-			// WriteResult<Question, String> result = coll.insert(myObject);
-			// String id = result.getSavedId();
-			// MyObject savedObject = coll.findOneById(id);
-			
-			// Hay que mirar como trabajar con MongoJack y conseguir convertir
-			// la lista de preguntas a JSON, para luego enviar a la DB.
-			
-			// Probablemente necesitemos un par de clases más.
 
 			BasicDBObject query = new BasicDBObject();
 			query.put("id", 1001);
