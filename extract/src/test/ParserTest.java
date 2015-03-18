dbae773d5bf339c2e88542cdad6a3e64dab34b2a
Mario_german <germanicopascual@gmail.com>
2015-03-18 20:26:29 +0100

    more

---
+++ extract/src/test/ParserTest.java
@@ -0,0 +1,38 @@
+package es.uniovi.asw.trivial;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import es.uniovi.asw.parser.ParserGIFT;
+import es.uniovi.asw.quizStructure.Answer;
+
+public class ParserTest {
+
+	@Test
+	public void testParse() {
+		String file="data/question.gift";
+		List<Answer> list = new ArrayList<Answer>();
+		
+		list.add(new Answer("Hideaki Anno",true));
+		list.add(new Answer("Toriyama",false));
+		
+		ParserGIFT parse = new ParserGIFT(file);
+		parse.parse();
+		assertEquals(parse.questions.size(), 1);
+		assertEquals(parse.questions.get(0).getTitle(), "Title");
+		assertEquals(parse.questions.get(0).getAnswers().get(0).getText(), list.get(0).getText());
+		assertEquals(parse.questions.get(0).getAnswers().get(1).getText(), list.get(1).getText());
+
+
+		assertEquals(parse.questions.get(0).getAnswers().get(0).isCorrectAnswer(), list.get(0).isCorrectAnswer());
+		assertEquals(parse.questions.get(0).getAnswers().get(1).isCorrectAnswer(), list.get(1).isCorrectAnswer());
+		
+				
+		
+	}
+
+}
